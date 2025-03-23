dede79aefba12ecd019ade72bc04aa8899323d95
wishwadev129 <wishwakankanamge129@gmail.com>
2025-03-23 16:38:21 +0530

    async progress

--- async_eg/src/main.rs
+++ async_eg/src/main.rs
@@ -1,46 +1,50 @@
 extern crate trpl; // required for mdbook test
 
-use std::time::Duration;
+use std::{pin::pin, time::Duration};
+
+use trpl::{ReceiverStream, Stream, StreamExt};
 
 fn main() {
     trpl::run(async {
-        let (tx, mut rx) = trpl::channel();
+        let mut messages =
+            pin!(get_messages().timeout(Duration::from_millis(200)));
 
-        let tx1 = tx.clone();
-        let tx1_fut = async move {
-            let vals = vec![
-                String::from("hi"),
-                String::from("from"),
-                String::from("the"),
-                String::from("future"),
-            ];
-
-            for val in vals {
-                tx1.send(val).unwrap();
-                trpl::sleep(Duration::from_millis(500)).await;
+        while let Some(result) = messages.next().await {
+            match result {
+                Ok(message) => println!("{message}"),
+                Err(reason) => eprintln!("Problem: {reason:?}"),
             }
-        };
+        }
+    })
+}
 
-        let rx_fut = async {
-            while let Some(value) = rx.recv().await {
-                println!("received '{value}'");
-            }
-        };
+fn get_messages() -> impl Stream<Item = String> {
+    let (tx, rx) = trpl::channel();
 
-        let tx_fut = async move {
-            let vals = vec![
-                String::from("more"),
-                String::from("messages"),
-                String::from("for"),
-                String::from("you"),
-            ];
+    trpl::spawn_task(async move {
+        let messages = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+        for (index, message) in messages.into_iter().enumerate() {
+            let time_to_sleep = if index % 2 == 0 { 100 } else { 300 };
+            trpl::sleep(Duration::from_millis(time_to_sleep)).await;
 
-            for val in vals {
-                tx.send(val).unwrap();
-                trpl::sleep(Duration::from_millis(1500)).await;
-            }
-        };
-
-        trpl::join3(tx1_fut, tx_fut, rx_fut).await;
+            tx.send(format!("Message: '{message}'")).unwrap();
+        }
     });
+
+    ReceiverStream::new(rx)
+}
+
+fn get_intervals() -> impl Stream<Item = u32> {
+    let (tx, rx) = trpl::channel();
+
+    trpl::spawn_task(async move {
+        let mut count = 0;
+        loop {
+            trpl::sleep(Duration::from_millis(1)).await;
+            count += 1;
+            tx.send(count).unwrap();
+        }
+    });
+
+    ReceiverStream::new(rx)
 }
