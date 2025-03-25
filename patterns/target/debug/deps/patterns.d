e91f18495519fc473fe5937419620e8c9887fa8c
wishwadev129 <wishwakankanamge129@gmail.com>
2025-03-25 17:51:34 +0530

    patterns

---
+++ patterns/target/debug/deps/patterns.d
@@ -0,0 +1,5 @@
+C:\projects\patterns\target\debug\deps\patterns.exe: src\main.rs
+
+C:\projects\patterns\target\debug\deps\patterns.d: src\main.rs
+
+src\main.rs:
---
+++ patterns/target/debug/patterns.d
@@ -0,0 +1,1 @@
+C:\projects\patterns\target\debug\patterns.exe: C:\projects\patterns\src\main.rs
