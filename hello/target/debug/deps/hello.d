ae9a02ea234abb79a1b2d1131b38d863e0b2ccc6
wishwadev129 <wishwakankanamge129@gmail.com>
2025-03-27 20:16:03 +0530

    rust http server

---
+++ hello/target/debug/deps/hello.d
@@ -0,0 +1,5 @@
+C:\projects\hello\target\debug\deps\hello.exe: src\main.rs
+
+C:\projects\hello\target\debug\deps\hello.d: src\main.rs
+
+src\main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+C:\projects\hello\target\debug\hello.exe: C:\projects\hello\src\main.rs
